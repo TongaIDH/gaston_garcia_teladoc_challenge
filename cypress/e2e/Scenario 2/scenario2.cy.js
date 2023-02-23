4af49ab8d900aab4f3184a480b743727691df40d
GastonGarciaAlvarez <galvarez@mobilecomputing.com.ar>
2023-02-23 13:37:16 -0300

    added fixture for scenario 2

--- cypress/e2e/Scenario 2/scenario2.cy.js
+++ cypress/e2e/Scenario 2/scenario2.cy.js
@@ -4,24 +4,21 @@ describe('Scenario 2', () => {
     })
 
     it('Should delete a user of the table', () => {
-        const firstName = 'Mark'
-        const lastName = 'Novak'
-        const userName = 'novak'
-        const role = 'Customer'
-        const email = 'asa@asd.cz'
-        const cellphone = '777888444'
+        cy.fixture('existingUser').then((user) => {
+            cy.searchForUser(user.firstName)
 
-        cy.searchForUser(firstName)
-
-        cy.assertCreatedUser(firstName, lastName, userName, role, email, cellphone)
-
+            cy.assertCreatedUser(user.firstName, user.lastName, user.userName, user.role, user.email, user.cellphone)
+        })
+        
         cy.cleanSearchInput()
 
-        cy.deleteUserByUsername(userName)
+        cy.fixture('existingUser').then((user) => {
+            cy.deleteUserByUsername(user.userName)
 
-        cy.searchForUser(userName)
-
-        cy.assertUserNotExists(userName)
+            cy.searchForUser(user.userName)
+    
+            cy.assertUserNotExists(user.userName)
+        })
 
         cy.cleanSearchInput()
     });
