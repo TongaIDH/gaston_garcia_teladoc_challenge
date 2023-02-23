9286064787cd354c673d6a1dfb21589c9884dd15
GastonGarciaAlvarez <galvarez@mobilecomputing.com.ar>
2023-02-23 13:45:28 -0300

    minor fixes on scripts and specs

--- cypress/e2e/Scenario 1/scenario1.cy.js
+++ cypress/e2e/Scenario 1/scenario1.cy.js
@@ -6,12 +6,9 @@ describe('Scenario 1', () => {
     it('Should add a user in the table', () => {
         cy.fixture('newUser').then((user) => {
             cy.addNewUser(user.firstName, user.lastName, user.userName, user.password, user.email, user.cellphone)
-
             cy.searchForUser(user.firstName)
-
             cy.assertCreatedUser(user.firstName, user.lastName, user.userName, user.role, user.email, user.cellphone)
-        })
-        
+        })    
         cy.cleanSearchInput()
     });
 })
--- cypress/e2e/Scenario 2/scenario2.cy.js
+++ cypress/e2e/Scenario 2/scenario2.cy.js
@@ -6,20 +6,14 @@ describe('Scenario 2', () => {
     it('Should delete a user of the table', () => {
         cy.fixture('existingUser').then((user) => {
             cy.searchForUser(user.firstName)
-
             cy.assertCreatedUser(user.firstName, user.lastName, user.userName, user.role, user.email, user.cellphone)
         })
-        
         cy.cleanSearchInput()
-
         cy.fixture('existingUser').then((user) => {
             cy.deleteUserByUsername(user.userName)
-
             cy.searchForUser(user.userName)
-    
             cy.assertUserNotExists(user.userName)
         })
-
         cy.cleanSearchInput()
     });
 })
