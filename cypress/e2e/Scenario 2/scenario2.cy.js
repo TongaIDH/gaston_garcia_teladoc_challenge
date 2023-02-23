2854cbab6332f0091f2725e5fb19b04d629a2585
GastonGarciaAlvarez <galvarez@mobilecomputing.com.ar>
2023-02-23 01:23:45 -0300

    finished scenario 2

--- cypress/e2e/Scenario 2/scenario2.cy.js
+++ cypress/e2e/Scenario 2/scenario2.cy.js
@@ -2,5 +2,32 @@ describe('Scenario 2', () => {
     it('Should delete a user of the table', () => {
         //Visiting sandbox page
         cy.visit('https://www.way2automation.com/angularjs-protractor/webtables/')
+        
+        // Asserting that the user novak really exists
+        cy.get('[table-title="Smart Table example"]')
+            .find('tr')
+            .eq(5)
+            .find('td')
+            .eq(2)
+            .should('have.text', 'novak')
+
+        // Deleting the user novak
+        cy.get('[table-title="Smart Table example"]')
+            .find('tr')
+            .eq(5)
+            .find('td')
+            .eq(10)
+            .children()
+            .click()
+        cy.get('.btn-primary')
+            .click()
+
+        // Asserting that the User Name novak doesn't exists anymore
+        cy.get('[table-title="Smart Table example"]')
+            .find('td')
+            .each(($el) => {
+                const data = $el.text()
+                expect(data).to.not.equal('novak')
+            })
     });
 })
